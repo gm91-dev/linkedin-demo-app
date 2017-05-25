54d4170aaaa07f09231baf9fa34c8c8a535831fb
Giulio Montenero <giuliomontenero@giulios-mbp.mul.ie.ibm.com>
2017-05-25 17:27:54 +0100

    chuck norris api added to the project - main page

--- app.js
+++ app.js
@@ -29,13 +29,6 @@ app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-/*
-app.get('/info/linkedin', function(req, res){
-  //console.log(path.join(__dirname + '/public/linkedin.html'));
-  res.sendFile(path.join(__dirname + '/public/linkedin.html'));
-});
-*/
-
 const options_auth = {
   url: 'https://api.linkedin.com/v1/people/~?format=json',
   method: 'GET',
@@ -49,76 +42,8 @@ request (options_auth, function (err, res, body) {
   console.log(json.headline);
 });
 
-/*
-//Facebook API example
-var propertiesObject_FB = { id:'967601343274222', name:'Giulio Montenero', access_token:'EAACEdEose0cBALo5Kmwk7qaMrswrCL9grLGwzuCdOjb9QkUXh4TE3V4fVutAuIsfxsjyNaF0YzR9743Xkamyn9RsL7QXZCX5HnMcQcIH386qKimz7MDg2CgL3zy3FZC7mqqXYa6cPzaToNXSrVKHbk5k6ffdlDZC84DlU8OCZCveaEgtlFRzmOtbxR56dVsZD' };
-
-request({url:'https://graph.facebook.com/', qs:propertiesObject_FB}, function(err, response, body) {
-  if(err) {
-    console.log(err);
-    return;
-  }
-  console.log(body);
-  console.log("Get response: " + response.statusCode);
-});
-
-*/
-
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-// compose for mysql code
-var dbcontroller = require('./controller/compose-mysql-connection');
-dbcontroller.databaseconnection();
-
-
-
-const util = require('util');
-// and so is assert
-const assert = require('assert');
-
-var mysql = require('mysql');
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// Within the application environment (appenv) there's a services object
-var services = appEnv.services;
-
-// The services object is a map named by service so we extract the one for Compose for MySQL
-var mysql_services = services["compose-for-mysql"];
-
-// This check ensures there is a services for MySQL databases
-assert(!util.isUndefined(mysql_services), "Must be bound to compose-for-mysql services");
-
-// We now take the first bound Compose for MySQL database service and extract it's credentials object
-var credentials = mysql_services[0].credentials;
-
-var connectionString = credentials.uri;
-
-// set up a new connection using our config details
-var connection = mysql.createConnection(credentials.uri);
-
-
-//reading from the database
-app.get("/read_fb_info", function(request, response) {
-
-  // execute a query on our database
-  connection.query('SELECT * FROM fb_info_table ORDER BY name ASC', function (err, result) {
-    if (err) {
-      console.log(err);
-     response.status(500).send(err);
-    } else {
-      console.log(result);
-     response.send(result);
-    }
-
-  });
-});
-
-//////////////////////////////////////////////////////////////////////////////////////
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,12 @@
-// routes.js
-
-var dbcontroller = require('../controller/compose-mysql-connection');
+// routes
 var path = require('path');
 var request = require('request');
 var Q = require('q');
 
-/*
-module.exports = function (app) {
-  app.get('/info/linkedin', function(req, res){
-    console.log(path.join(__dirname + '/public/linkedin.html'));
-    //res.sendFile(path.join(__dirname + '/public/linkedin.html'));
-    res.sendFile(path.join(__dirname, '../public', 'linkedin.html'));
-  });
-};
-*/
-
 retrieveLinkedInUserInfo = function() {
 	var deferred = Q.defer();
-
   //retrieve the necessary information at first
   var json = null;
-
   const options_auth = {
     url: 'https://api.linkedin.com/v1/people/~?format=json',
     method: 'GET',
@@ -28,10 +14,9 @@ retrieveLinkedInUserInfo = function() {
       'Authorization': 'Bearer AQV_fc2Js9rs9iB4N7U-xXGI63a4A3099vPhYlTIWFhar9SfjZdH3H4-6O1UAU_f7OvOeCfh4VvJzVzzoZ0HImla9R_eKxzTBjhZtFA6c0Lbx_s9SheIAIc9_l_1VVhxLoI1W7JMs0gCD0lC1Bdz4FLkqHETGWEO766qtEzNctFw_mNfaxc'
     }
   };
-
   request (options_auth, function (err, res, body) {
     if(err){
-      deferred.resolve(null);
+      deferred.reject();
     }
     else{
       json = JSON.parse(body);
@@ -39,96 +24,24 @@ retrieveLinkedInUserInfo = function() {
       console.log(json.headline);
     }
   });
-
 	return deferred.promise;
 };
-
-
-
-
-/*
-
-// Util is handy to have around, so thats why that's here.
-const util = require('util');
-// and so is assert
-const assert = require('assert');
-
-var mysql = require('mysql');
-
-var cfenv = require('cfenv');
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// Within the application environment (appenv) there's a services object
-var services = appEnv.services;
-
-// The services object is a map named by service so we extract the one for Compose for MySQL
-var mysql_services = services["compose-for-mysql"];
-
-// This check ensures there is a services for MySQL databases
-assert(!util.isUndefined(mysql_services), "Must be bound to compose-for-mysql services");
-
-// We now take the first bound Compose for MySQL database service and extract it's credentials object
-var credentials = mysql_services[0].credentials;
-
-var connectionString = credentials.uri;
-
-// set up a new connection using our config details
-var connection = mysql.createConnection(credentials.uri);
-
-/*
-//delete database table
-connection.connect(function(err) {
-  if (err) {
-   console.log(err);
-  } else {
-    connection.query('DROP TABLE fb_info_table', function (err, result) {      if (err) {
-        console.log(err)
-      }
-    });
-  }
-});
-*/
-
-/*
-connection.connect(function(err) {
-  if (err) {
-   console.log(err);
-  } else {
-    connection.query('CREATE TABLE fb_info_table (id int auto_increment primary key, name varchar(256) NOT NULL, surname varchar(256) NOT NULL, email varchar(256) NOT NULL, role varchar(256) NOT NULL)', function (err,result){
-      if (err) {
-        console.log(err)
-      }
-    });
-  }
-});
-
-*/
-
-
-
-
-
-
 
 
 retrieveFacebookUserInfo = function() {
 	var deferred = Q.defer();
 
-	var propertiesObject_FB = { id:'967601343274222', name:'Giulio Montenero', access_token:'EAACEdEose0cBAFPwMHIZCUeepBsoaMB1WtgNPqZBiLxmsr4iKAVkO2rBeYtDLlX6WO8ryxe9GCGDZCRoLXL493x1jAO1RdOLLFCVZBYYFXnSVIqZAT6RmcHDB4Su4xVJCEUZCTfJS5TVqKlOlkY6RVtDLMKX71THcVoJrcIgaMTPLmqnksrvcEKoB86SzYu4AZD' };
+	var propertiesObject_FB = { id:'967601343274222', name:'Giulio Montenero', access_token:'EAACEdEose0cBAIIoeCVwo3ci7qhPtCYPcx7F3sCQBzw4WX8ssRu1EgJuhmaTWScZAIr18EZBe5PqLggwt8Qo6TYHClPdNX44fVcfUGWdRdXJXVOHGZCnof1axUt8ujRAZCmoVa2sj2Bm4Q9Fq2e9ejijqtKOZBjwZB3WBwQZB4kEzCz0PWUZCty6AnNXHyQGtqMZD' };
+	//var propertiesObject_FB = { id:'1528680640499620', name:'Giulio Montenero', access_token:'EAAGDZB2SXKbYBAKn5cV7nUlQiz4GN0U1M9N0mql8vFTNd45eBlhUlCgHxcCaR1LsUsPhI7c3xxdBJT8vHW0HsZA4iCZB8uroGIkKnHFVIeRUnfRMlacNfI3opk9yDOZAGYv3IaDACCj1hJcXAxxpahlQlmXQLe8ZD' };
 
 	request({url:'https://graph.facebook.com/', qs:propertiesObject_FB}, function(err, response, body) {
 	  if(err) {
-			deferred.resolve(null);
+			deferred.reject();
 	  }
 		else {
 			var fb_json = JSON.parse(body);
 		  console.log("Get response: " + response.statusCode);
 			console.log(fb_json);
-
-			//storing information to db
-			//dbcontroller.writingtodb();
 
 			deferred.resolve(fb_json);
 		}
@@ -137,26 +50,40 @@ retrieveFacebookUserInfo = function() {
 	return deferred.promise;
 };
 
+getphraseoftheday = function() {
+	var deferred = Q.defer();
+
+	request({url:'https://api.chucknorris.io/jokes/random'}, function(err, response, body) {
+	  if(err) {
+			deferred.reject();
+	  }
+		else {
+		  console.log("Get response: " + response.statusCode);
+			console.log(body);
+			deferred.resolve(body);
+		}
+	});
+
+	return deferred.promise;
+};
+
+
 module.exports = function (app) {
 
   // set up the routes themselves
   /*
 	app.get("/", function (req, res) {
-      // do stuff
+      // do something
   });
 	*/
 
   app.get('/info/facebook', function(req, res){
 		retrieveFacebookUserInfo().then(function(result){
-			console.log("ENTERED");
-
 				res.render('facebook.ejs', {
 					title : 'Facebook information',
 					fb_obj: result
 				});
-
 			});
-
 		});
 
   app.get('/info/linkedin', function(req, res){
@@ -168,4 +95,11 @@ module.exports = function (app) {
 		});
   });
 
-}
+	app.get('/curiosity', function(req, res){
+		getphraseoftheday().then(function(result){
+			console.log(result);
+			res.send(result);
+		});
+  });
+
+};
