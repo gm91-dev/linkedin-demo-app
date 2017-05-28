daf008ece24817d00eac3d6c31f8cf165094dc45
Giulio Montenero <giuliomontenero@Giulios-MacBook-Pro.local>
2017-05-28 15:12:02 +0100

    last version

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ retrieveLinkedInUserInfo = function() {
 retrieveFacebookUserInfo = function() {
 	var deferred = Q.defer();
 
-	var propertiesObject_FB = { id:'967601343274222', name:'Giulio Montenero', access_token:'EAACEdEose0cBAIIoeCVwo3ci7qhPtCYPcx7F3sCQBzw4WX8ssRu1EgJuhmaTWScZAIr18EZBe5PqLggwt8Qo6TYHClPdNX44fVcfUGWdRdXJXVOHGZCnof1axUt8ujRAZCmoVa2sj2Bm4Q9Fq2e9ejijqtKOZBjwZB3WBwQZB4kEzCz0PWUZCty6AnNXHyQGtqMZD' };
+	var propertiesObject_FB = { id:'967601343274222', name:'Giulio Montenero', access_token:'EAACEdEose0cBAGrYKUVgcWZCtSjLUBRoK5IddQEUzrk0VYvOgo8KiOBbQod52tC2s2BMvJEZBj6KFJCSGj0zIQZAgbLIBfK1g9ZCtF22LgEUwWoaHIQFeXF5r8GYqQJ4GhqxwZCKB4dm92ZBppGORY0MBceSwQwxmmhpk7hP995GZAVEOx7xk5ul8ZCuxt5AvToZD' };
 	//var propertiesObject_FB = { id:'1528680640499620', name:'Giulio Montenero', access_token:'EAAGDZB2SXKbYBAKn5cV7nUlQiz4GN0U1M9N0mql8vFTNd45eBlhUlCgHxcCaR1LsUsPhI7c3xxdBJT8vHW0HsZA4iCZB8uroGIkKnHFVIeRUnfRMlacNfI3opk9yDOZAGYv3IaDACCj1hJcXAxxpahlQlmXQLe8ZD' };
 
 	request({url:'https://graph.facebook.com/', qs:propertiesObject_FB}, function(err, response, body) {
